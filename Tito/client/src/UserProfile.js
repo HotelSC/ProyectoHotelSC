d66679aabc1355c3e5add0e5e19ad9bbaeb0c4d2
TitoAaron <titoaaron13@gmail.com>
2022-09-20 13:53:55 +0200

    added snail

--- Tito/client/src/UserProfile.js
+++ Tito/client/src/UserProfile.js
@@ -1,49 +1,60 @@
 import { useEffect, useState } from 'react'
 import prof from './img/employeeIcon.png'
 import logo from './img/logo.png'
+import snail  from './img/snail.gif'
 
 export default function UserProfile(props) {
+    const data = props.userData.data;
     while(props.userData.data == undefined){
         return(
-            <div><p>CARGANDOOOOOOOOOOOOOO</p></div>
+            <div><img className='snail' src={snail}/></div>
         )
-    }return (
+    }
+    return (
+        
         <div className="container-profile container">
+            
                 <img className="logo" src={logo}/>
                 <img className="pic" src={prof}/>
-                <h1>LOREM IPSUM</h1>
-            <div className="userInfo">
+                <h1>{data.usuario} - {data.categoria}</h1> 
+            
+                <div className="userInfo">
                 <div className="row">
                     <div className="col">
                         <span >Nombre/s</span>
-                        <p>{props.userData.data.nombre}</p>
+                        <p>{data.nombre}</p>
                     </div>
                     <div className="col">
                         <span >Apellido/s</span>
-                        <p>LOREM IPSUM</p>
+                        <p>{data.apellidos}</p>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col">
-                        <span >Telefono</span>
-                        <p>LOREM IPSUM</p>
+                        <span >DNI</span>
+                        <p>{data.dni}</p>
                     </div>
                     <div className="col">
-                        <span >Correo</span>
-                        <p>LOREM IPSUM</p>
+                        <span >Fecha de nacimiento</span>
+                        <p> {data.fechaNacimiento}</p>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col">
-                        <span >Telefono</span>
-                        <p>LOREM IPSUM</p>
+                        <span >direccion</span>
+                        <p>{data.direccion}</p>
                     </div>
                     <div className="col">
-                        <span >Fecha de Nacimiento</span>
-                        <p>Lorem Ipsum</p>
+                        <span >Localidad</span>
+                        <p>{data.localidad}</p>
                     </div>
                 </div>
-
+                <div className="row">
+                    <div className="col">
+                        <span >Email</span>
+                        <p>{data.email}</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
