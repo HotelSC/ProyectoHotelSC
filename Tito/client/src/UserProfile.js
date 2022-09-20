2718720577d5835489ac22672d8d3e17485ac3c9
TitoAaron <titoaaron13@gmail.com>
2022-09-20 10:41:59 +0200

    UserProfile

--- Tito/client/src/UserProfile.js
+++ Tito/client/src/UserProfile.js
@@ -1,19 +1,49 @@
-
+import { useEffect, useState } from 'react'
 import prof from './img/employeeIcon.png'
 import logo from './img/logo.png'
 
-export default function Profile(props) {
-    return (
+export default function UserProfile(props) {
+    while(props.userData.data == undefined){
+        return(
+            <div><p>CARGANDOOOOOOOOOOOOOO</p></div>
+        )
+    }return (
         <div className="container-profile container">
                 <img className="logo" src={logo}/>
                 <img className="pic" src={prof}/>
                 <h1>LOREM IPSUM</h1>
             <div className="userInfo">
-                <span>Nombre/s</span>
-                <p>LOREM IPSUM</p>
-                <span>Apellido/s</span>
-                <p>LOREM IPSUM</p>
-                <span>Telefono</span>
+                <div className="row">
+                    <div className="col">
+                        <span >Nombre/s</span>
+                        <p>{props.userData.data.nombre}</p>
+                    </div>
+                    <div className="col">
+                        <span >Apellido/s</span>
+                        <p>LOREM IPSUM</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <span >Telefono</span>
+                        <p>LOREM IPSUM</p>
+                    </div>
+                    <div className="col">
+                        <span >Correo</span>
+                        <p>LOREM IPSUM</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <span >Telefono</span>
+                        <p>LOREM IPSUM</p>
+                    </div>
+                    <div className="col">
+                        <span >Fecha de Nacimiento</span>
+                        <p>Lorem Ipsum</p>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
